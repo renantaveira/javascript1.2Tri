85c8318465de40c022dfa172e3edfa84e193df29
renantaveira <renan.taveira@escola.pr.gov.br>
2026-03-27 07:56:53 -0300

    update

---
+++ 27032026.js
@@ -0,0 +1,14 @@
+// Criar Array 
+
+const fila = [1,2,3,4,5,6];
+
+const filaStringAmigos = ["João Victor","Pedro","Luis Miguel","Rafael","Anao P.","Raquely Formiguinha","Larissa rabids"]
+
+function lerAmigos(filaStringAmigos) 
+   console.log(`Você tem ${filaStringAmigos.length} amigos`){
+ 
+   for(let i = 0; i < filaStringAmigos.length;i++){
+     console.log(filaStringAmigos[i] === "Anao P.")
+       
+   }
+}
